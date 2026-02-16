bac192bdf1aaef7383f099f7d73991279ee9e6c9
Gabriel Padilla <g.padilla@uniandes.edu.co>
2026-02-16 15:00:53 -0500

    big change! modular

--- app/page.tsx
+++ app/page.tsx
@@ -1,92 +1,18 @@
-import React from "react";
-import Image from "next/image";
-import DataTable from "@/components/DataTable";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
-import { TrendingDown, TrendingUp } from "lucide-react";
-import { fetcher } from "@/lib/coingecko.actions";
-
-const columns: DataTableColumn<TrendingCoin>[] = [
-  {
-    header: "Name",
-    cellClassName: "name-cell",
-    cell: (coin) => {
-      const item = coin.item;
-
-      return (
-        <Link href={`/coins/${item.id}`}>
-          <Image src={item.large} alt={item.name} width={36} height={36} />
-          <p>{item.name}</p>
-        </Link>
-      );
-    },
-  },
-  {
-    header: "24h Change",
-    cellClassName: "name-cell",
-    cell: (coin) => {
-      const item = coin.item;
-      const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
-
-      return (
-        <div
-          className={cn(
-            "price-change",
-            isTrendingUp ? "text-green-500" : "text-red-500",
-          )}
-        >
-          <p>
-            {isTrendingUp ? (
-              <TrendingUp width={16} height={16} />
-            ) : (
-              <TrendingDown width={16} height={16} />
-            )}
-            {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
-          </p>
-        </div>
-      );
-    },
-  },
-
-  {
-    header: "Price",
-    cellClassName: "price-cell",
-    cell: (coin) => `$${coin.item.data.price}`,
-  },
-];
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import React, { Suspense } from "react";
 
 const page = async () => {
-  const coin = await fetcher<CoinDetailsData>("/coins/bitcoin");
   return (
-    <main className="main-container">
+    <main>
       <section className="home-grid">
-        <div id="coin-overview">
-          <div className="header pt-2">
-            <Image
-              src={coin.image.large}
-              alt={coin.name}
-              width={56}
-              height={56}
-            />
+        <Suspense fallback={<div>Loading Overview...</div>}>
+          <CoinOverview />
+        </Suspense>
 
-            <div className="info">
-              <p>
-                {coin.name} / {coin.symbol.toUpperCase()}
-              </p>
-              <h1>${coin.market_data.current_price.usd.toLocaleString()} </h1>
-            </div>
-          </div>
-        </div>
-        <p>Trending Coins</p>
-        <DataTable
-          data={[]}
-          columns={columns}
-          rowKey={(coin) => coin.item.id}
-        />
-      </section>
-
-      <section className="w-full mt-7 space-y-4">
-        <p>Categories</p>
+        <Suspense fallback={<div>Loading Trending...</div>}>
+          <TrendingCoins />
+        </Suspense>
       </section>
     </main>
   );
