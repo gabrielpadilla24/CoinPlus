ea62b5a32e2185732e263dd85b40754a1d417b6a
Gabriel Padilla <g.padilla@uniandes.edu.co>
2026-02-16 13:14:44 -0500

    pre fetch

--- app/page.tsx
+++ app/page.tsx
@@ -40,6 +40,7 @@ const columns: DataTableColumn<TrendingCoin>[] = [
             ) : (
               <TrendingDown width={16} height={16} />
             )}
+            {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
           </p>
         </div>
       );
